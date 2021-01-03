4776714eda98ff41586f7f1f3457b287502a56b1
lorisadjobi@gmail.com <Narcos75>
2021-01-03 20:06:48 +0100

    Frontend1.0

--- angular-project/src/app/movies/movies.component.ts
+++ angular-project/src/app/movies/movies.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movies',
@@ -7,13 +9,16 @@ import { Movie } from '../movie';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent implements OnInit {
-  hero: Movie = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  constructor() { }
+  movies: Movie[];
+
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+    this.getMovies();
   }
 
+  getMovies(): void {
+    this.movieService.getMovies()
+    .subscribe(movies => this.movies = movies);
+  }
 }
